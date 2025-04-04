bf191504cbadb3605ab8e90fa602f94b018f1dc7
p123g <Pri@123g>
2025-04-04 15:41:56 +0530

    basics completed

---
+++ app/src/main/java/com/smallbucket/cc_coroutinesbasicsforbeginners/GenericsVararg.kt
@@ -0,0 +1,32 @@
+package com.smallbucket.cc_coroutinesbasicsforbeginners
+
+fun main() {
+    val obj = Container(3)
+    val a: Int = obj.data // Int
+    val obj2 = Container<Int>(4)
+    obj2.getValue() // Int
+
+    val objs = Container<String>("hello")
+    objs.getValue()// string
+    val list: List<String> = listOf<String>()
+    add(1,2,3,5,7,8,8)  //34
+    add(1,1,1,1,1,1,1,1) //8
+}
+
+fun add(vararg values: Int) {
+    var sum = 0
+    for ( i in values){
+        sum +=i
+    }
+    println(sum)
+}
+
+class Container<T>(var data: T) {
+    fun setValue(value: T) {
+        data = value
+    }
+
+    fun getValue(): T {
+        return data
+    }
+}
---
+++ app/src/main/java/com/smallbucket/cc_coroutinesbasicsforbeginners/NestedOuterClass.kt
@@ -0,0 +1,18 @@
+package com.smallbucket.cc_coroutinesbasicsforbeginners
+
+fun main() {
+    var obj = Outer()
+    obj.i
+    val inner = Outer().Nested()
+    inner.test()// I am in nested class 0
+}
+
+class Outer {
+    var i = 0
+
+    inner class Nested {
+        fun test() {
+            println("I am in nested class $i") // No compile time error
+        }
+    }
+}
