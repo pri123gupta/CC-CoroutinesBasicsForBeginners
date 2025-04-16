642eec1ad2c858904520cf4f005c9b60b1768970
p123g <Pri@123g>
2025-04-16 23:00:16 +0530

    FlowOperatorsTerminalMapFilterBuffer Complete

--- app/src/main/java/com/smallbucket/cc_coroutinesbasicsforbeginners/my3flowschannels/FlowOperatorsTerminalMapFilterBufferActivity.kt
+++ app/src/main/java/com/smallbucket/cc_coroutinesbasicsforbeginners/my3flowschannels/FlowOperatorsTerminalMapFilterBufferActivity.kt
@@ -10,11 +10,19 @@ import com.smallbucket.cc_coroutinesbasicsforbeginners.my1coroutinescopeandconte
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
+import kotlin.system.measureTimeMillis
 
 class FlowOperatorsTerminalMapFilterBufferActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,27 +31,127 @@ class FlowOperatorsTerminalMapFilterBufferActivity : ComponentActivity() {
         setContent {
             basicTextview("Welcome to FlowOperatorsTerminalMapFilterBufferActivity")
         }
-        GlobalScope.launch(Dispatchers.Main) {
-            producers()
-                .onStart {
-                    Log.d(APP_TAG, "onStarting out  ") // when emitting and production is started,
+
+//        fewFunctions()
+        fnEmitManually()
+//        someMoreFns()
+//        someMoreFns2()
+//        fnsMapFilter()
+//        fnsMapFilterUserDefined()
+//        fnBuffer()
+    }
+}
+
+fun fnEmitManually() {
+    GlobalScope.launch(Dispatchers.Main) {
+        producers()
+            .onStart {
+                emit(-1)
+                Log.d(APP_TAG, "onStarting out  ") // when emitting and production is started,
                 // runs only once before all items are produced
-                }
-                .onCompletion {
-                    Log.d(
-                        APP_TAG,
-                        "onCompletion     "
-                    ) // After all items are emitted and completed
-                }.onEach {
-                    Log.d(
-                        APP_TAG,
-                        "onEach About to emit $it  "
-                    ) // When single item is about to emit, it is called
-                }
+            }
+            .onCompletion {
+                emit(6)
+                Log.d(
+                    APP_TAG,
+                    "onCompletion     "
+                ) // After all items are emitted and completed
+            }.onEach {
+                Log.d(
+                    APP_TAG,
+                    "onEach About to emit $it  "
+                ) // When single item is about to emit, it is called
+            }
+            .collect {
+                Log.d(APP_TAG, "collect: $it")
+            }
+    }
+}
+
+private fun fnBuffer() {
+    GlobalScope.launch(Dispatchers.Main) {
+        val time = measureTimeMillis {
+            producer()
+                .buffer(3) // random value
                 .collect {
-                    Log.d(APP_TAG, "collect: $it")
+                    delay(1500)
+                    Log.d(APP_TAG, "collected = $it")  //1,2,3
                 }
         }
+        Log.d(APP_TAG, "time = $time")  //
+    }
+}
+
+private fun fnsMapFilterUserDefined() {
+    GlobalScope.launch(Dispatchers.Main) {
+        getNotes()
+            .map {
+                FormattedNote(
+                    isActive = it.isActive,
+                    title = it.title.uppercase(),
+                    description = it.description
+                )
+            }.filter { it.isActive }
+            .collect {
+                Log.d(APP_TAG, "Collected = $it")
+            }
+    }
+}
+
+data class Note(val id: Int, val isActive: Boolean, val title: String, val description: String)
+data class FormattedNote(val isActive: Boolean, val title: String, val description: String)
+
+private fun getNotes(): Flow<Note> {
+    val list = listOf(
+        Note(1, true, "First", "First desc"),
+        Note(2, true, "Second", "Second desc"),
+        Note(3, false, "Third", "Third desc")
+    )
+    return list.asFlow()
+}
+
+private fun fnsMapFilter() {
+    GlobalScope.launch(Dispatchers.Main) {
+        val result: List<Int> = producers().toList() // Assembles all elements to a list
+        Log.d(APP_TAG, "List = $result")
+    }
+}
+
+private fun someMoreFns2() {
+    GlobalScope.launch(Dispatchers.Main) {
+        val result: List<Int> = producers().toList() // Assembles all elements to a list
+        Log.d(APP_TAG, "List = $result")
+    }
+}
+
+private fun someMoreFns() {
+    GlobalScope.launch(Dispatchers.Main) {
+        val result: Int = producers().first() // First element emitted
+        Log.d(APP_TAG, "first = $result")
+    }
+}
+
+private fun fewFunctions() {
+    GlobalScope.launch(Dispatchers.Main) {
+        producers()
+            .onStart {
+                Log.d(APP_TAG, "onStarting out  ") // when emitting and production is started,
+                // runs only once before all items are produced
+            }
+            .onCompletion {
+                Log.d(
+                    APP_TAG,
+                    "onCompletion     "
+                ) // After all items are emitted and completed
+            }.onEach {
+                Log.d(
+                    APP_TAG,
+                    "onEach About to emit $it  "
+                ) // When single item is about to emit, it is called
+            }
+            .collect {
+                Log.d(APP_TAG, "collect: $it")
+            }
     }
 }
 
@@ -51,7 +159,6 @@ fun producers() = flow<Int> {
     val list = listOf(1, 2, 3, 4, 5)
     list.forEach {
         delay(1000)
-        Log.d(APP_TAG, "produced $it")
         emit(it)
     }
 }
